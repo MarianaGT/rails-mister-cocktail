8974c9b005c829413dc9765955c2b9b10664ba7d
Mariana Galan <gatm870125@gmail.com>
2020-06-11 13:49:52 -0500

    Cocktail controller with index, show, new and create working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [:index, :show, :new, :create]
+
+  #do
+  #   resources :ingredients, only: [:show]
+  #   resources :dose, only: [:new, :create]
+  # end
+  # resources :dose, only: [:delete]
 end
