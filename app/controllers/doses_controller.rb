0c898384063f29eb6db6dc1a6d9c22e42e7bb276
Mariana Galan <gatm870125@gmail.com>
2020-06-11 20:11:47 -0500

    Delete action works

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -17,9 +17,8 @@ class DosesController < ApplicationController
 
   def destroy
     @dose = Dose.find(params[:id])
-    @cocktail = @dose.cocktail
     @dose.destroy
-    redirect_to cocktail_path(@cocktail)
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
